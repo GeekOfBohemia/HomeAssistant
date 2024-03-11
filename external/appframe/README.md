465e9e1a754707c3e2f3ece8d177798f57735960
GeekOfBohemia <162793317+GeekOfBohemia@users.noreply.github.com>
2024-03-11 20:43:26 +0100

    Add files via upload

--- external/appframe/README.md
+++ external/appframe/README.md
@@ -1,1 +1,1 @@
-
+# appframe
---
+++ external/appframe/config_data.py
@@ -0,0 +1,47 @@
+import os
+import sys
+from dataclasses import dataclass
+import configparser
+
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from appframe.helpers import HelperOp
+
+
+@dataclass
+class ConfigData:
+    inverter_ip: str
+    inverter_port: int
+    inverter_sn: int
+    mqtt_basic: str
+    mqtt_server: str
+    mqtt_port: int
+    mqtt_topic: str
+    mqtt_username: str
+    ha_mqtt_topic: str
+
+    def __init__(self):
+        configParser = configparser.RawConfigParser()
+        data_file_path = HelperOp.filename("config.cfg")
+        configParser.read(data_file_path)
+
+        self.inverter_ip = configParser.get("SofarInverter", "inverter_ip")
+        self.inverter_port = int(configParser.get("SofarInverter", "inverter_port"))
+        self.inverter_sn = int(configParser.get("SofarInverter", "inverter_sn"))
+        self.mqtt_server = configParser.get("MQTT", "mqtt_server")
+        self.mqtt_port = int(configParser.get("MQTT", "mqtt_port"))
+        self.mqtt_username = configParser.get("MQTT", "mqtt_username")
+        self.mqtt_passwd = configParser.get("MQTT", "mqtt_passwd")
+        self.ha_ip = configParser.get("HomeAssistant", "ip")
+        self.ha_port = configParser.get("HomeAssistant", "port")
+        self.ha_url = f"http://{self.ha_ip}:{self.ha_port}/"
+        # self.ha_url = f"https://{self.ha_ip}:{self.ha_port}/"
+        self.ha_api_authorization = configParser.get(
+            "HomeAssistant", "api_authorization"
+        )
+        self.app_framework = configParser.get("HomeAssistant", "app_framework")
+
+
+configData: ConfigData = ConfigData()
+if __name__ == "__main__":
+    print(configData.ha_url)
+    print(configData.ha_api_authorization)
